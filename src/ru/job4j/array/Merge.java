cb0d21a1fee90ffce78fe6053722cedc21fc6a52
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2020-10-12 22:55:09 +0300

    Тестовое задание после экзамена "Базовый синтаксис"[сортировка слиянием]

---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+public class Merge {
+    public static int[] merge(int[] left, int[] right) {
+        int[] copyLeft = new int[left.length + 1];
+        int[] copyRight = new int[right.length + 1];
+        int[] rsl = new int[left.length + right.length];
+        for (int i = 0; i < left.length; i++) {
+            copyLeft[i] = left[i];
+        }
+        for (int j = 0; j < right.length; j++) {
+            copyRight[j] = right[j];
+        }
+        copyLeft[left.length] = Integer.MAX_VALUE;
+        copyRight[right.length] = Integer.MAX_VALUE;
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < rsl.length; k++) {
+            if (copyLeft[i] <= right[j]) {
+                rsl[k] = left[i];
+                i++;
+            } else {
+                rsl[k] = copyRight[j];
+                j++;
+            }
+        }
+        return rsl;
+    }
+}
