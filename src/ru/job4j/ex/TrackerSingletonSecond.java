a3787e65dcf4c668df14efb629d9f788c98f0567
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2020-12-24 20:21:44 +0300

    2. Singleton. [#367908]

---
+++ src/ru/job4j/ex/TrackerSingletonSecond.java
@@ -0,0 +1,82 @@
+package ru.job4j.ex;
+
+import ru.job4j.tracker.Item;
+import java.util.Arrays;
+
+public class TrackerSingletonSecond {
+    private static TrackerSingletonSecond instance;
+
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    private TrackerSingletonSecond() {
+    };
+    
+    public static TrackerSingletonSecond getInstance() {
+        if (instance == null) {
+            instance = new TrackerSingletonSecond();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] requiredName = new Item[size];
+        int length = 0;
+        for (int index = 0; index < size; index ++) {
+            Item item = items[index];
+            if (key.equals(item.getName())) {
+                requiredName[length] = item;
+                length++;
+            }
+        }
+        return Arrays.copyOf(requiredName, length);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            item.setId(id);
+            items[index] = item;
+        }
+        return rsl;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+        return rsl;
+    }
+}
