87e4cbc0e0990062565c28e4eb8f799f00b7b424
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2021-03-05 21:04:32 +0300

    Update directory

--- src/ru/job4j/objects/Doctor.java
+++
@@ -1,4 +0,0 @@
-package ru.job4j.objects;
-
-public class Doctor extends Profession {
-}
--- src/ru/job4j/oop/DummyDic.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.oop;
-
-public class DummyDic {
-    public String engToRus(String eng) {
-        String rus = "Неизвестное слово " + eng;
-        return rus;
-    }
-}
